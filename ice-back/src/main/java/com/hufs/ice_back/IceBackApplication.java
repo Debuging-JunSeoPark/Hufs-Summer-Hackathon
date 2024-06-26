c95b78909779fb6eba35ad605bdd505df0794eeb
이윤빈 <kanado20000@naver.com>
2024-06-26 16:53:50 +0900

    환경 설정

---
+++ ice-back/src/main/java/com/hufs/ice_back/IceBackApplication.java
@@ -0,0 +1,13 @@
+package com.hufs.ice_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceBackApplication.class, args);
+	}
+
+}
