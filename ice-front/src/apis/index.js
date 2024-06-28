e48da66a097b494cebfbeff8e85beb148d85fdb8
Debuging-JunSeoPark <pjseo1313@daum.net>
2024-06-29 08:56:00 +0900

    cd

--- ice-front/src/apis/index.js
+++ ice-front/src/apis/index.js
@@ -6,13 +6,8 @@ const API_DOMAIN = `${DOMAIN}/api`;
 
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
 const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
-<<<<<<< HEAD
-const MATCH_UP_URL = () => `${API_DOMAIN}/v1/article/list/filtered`;
-
-=======
 const SEARCH_NAME_URL = () => `${API_DOMAIN}/auth/name`;
 const POST_ARTICLE_URL = () => `${API_DOMAIN}/v1/article`;
->>>>>>> 2d584a9c3a4ec3903d7f16f43742946eea5a35da
 
 export const signInRequest = async (requestBody) => {
   //asyns를 통해 비동기 함수 정의
@@ -46,27 +41,6 @@ export const signUpRequest = async (requestBody) => {
   return result;
 };
 
-<<<<<<< HEAD
-};
-
-export const matchUpRequest = async (requestBody) => {
-    
-    const result = await axios.post(MATCH_UP_URL(), requestBody) //await은 요청의 응답이 돌아올 떄 까지 함수 실행을 멈추는 역할 한다(asyns함수 안에서만 사용가능)
-    .then(response => {
-
-        const responseBody = response.data;// responseBody의 key값들이: code, message, token, expirationTime
-        
-        return responseBody;
-    })
-    .catch(error => {
-        if (!error.response || !error.response.data) return null; //예상하지못한 error를 0으로 반환
-        const responseBody = error.response.data;                 //예상했던 error를 code와message로 반환
-        return responseBody;
-    });
-return result;
-
-};
-=======
 export const searchNameRequest = async (requestBody) => {
   //asyns를 통해 비동기 함수 정의
   const result = await axios
@@ -98,4 +72,3 @@ export const postArticleRequest = async (requestBody) => {
       });
     return result;
   };
->>>>>>> 2d584a9c3a4ec3903d7f16f43742946eea5a35da
