3b4205509fb97ea19064f1d506530c7e25b5f836
이윤빈 <kanado20000@naver.com>
2024-06-28 19:27:12 +0900

    토 시스템 추가

--- ice-back/src/main/java/com/hufs/ice_back/dto/request/SignUpRequestDto.java
+++ ice-back/src/main/java/com/hufs/ice_back/dto/request/SignUpRequestDto.java
@@ -1,8 +1,10 @@
 package com.hufs.ice_back.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
