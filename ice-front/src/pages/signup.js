eb09181d143af9b32efcd37709904aeaf15a7408
Debuging-JunSeoPark <pjseo1313@daum.net>
2024-06-29 06:55:21 +0900

    NE

--- ice-front/src/pages/signup.js
+++ ice-front/src/pages/signup.js
@@ -48,38 +48,12 @@ const SignUp = () => {
       <div className="flex flex-1 items-center justify-center">
         <div className="bg-white w-full max-w-lg py-10 px-5 rounded-lg text-center">
           <h3 className="text-3xl text-gray-800">Sign Up</h3>
-<<<<<<< HEAD
           <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <input type="text" value={career} onChange={(e) => setCareer(e.target.value)} placeholder="Career (if applicable)" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <select value={position} onChange={(e) => setPosition(e.target.value)} className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg">
             <option value="없음">Select Position</option>
-=======
-          <form className="flex flex-col mt-5">
-            <input
-              placeholder="Email"
-              name="Email"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="Password"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <input
-              type="text"
-              placeholder="Carrer (없을 시 작성 X)"
-              name="Carrer"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <select
-              name="Position"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            >
-              <option value="없음">포지션을 골라주세요</option>
->>>>>>> a51a8a65acc6aa6dbcfa1377c98b67f9ecc3d116
               <option value="catcher">포수</option>
               <option value="pitcher">투수</option>
               <option value="first_base">1루수</option>
@@ -90,7 +64,6 @@ const SignUp = () => {
               <option value="center_fielder">중견수</option>
               <option value="right_fielder">우익수</option>
             </select>
-<<<<<<< HEAD
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Age" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <input type="text" value={sex} onChange={(e) => setSex(e.target.value)} placeholder="Sex" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
@@ -98,46 +71,6 @@ const SignUp = () => {
             <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} placeholder="Height" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} placeholder="Weight" className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg" />
             <button type="submit" className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
-=======
-            <input
-              type="text"
-              placeholder="Name"
-              name="Name"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <input
-              type="number"
-              placeholder="Age"
-              name="Age"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <select
-              name="Sex"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            >
-              <option value="MALE">남성</option>
-              <option value="FEMALE">여성</option>
-            </select>
-            <input
-              type="text"
-              placeholder="Team (없을 시 작성 X)"
-              name="Team"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <input
-              type="number"
-              placeholder="Height"
-              name="Height"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <input
-              type="number"
-              placeholder="Weight"
-              name="Weight"
-              className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-            />
-            <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
->>>>>>> a51a8a65acc6aa6dbcfa1377c98b67f9ecc3d116
               Submit
             </button>
           </form>
